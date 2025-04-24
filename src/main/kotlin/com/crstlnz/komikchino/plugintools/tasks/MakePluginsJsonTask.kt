051c5b9bed6a3ea982b4a23e486b4ab90ea288cd
Crytalenz <kelvinzzzzzzzzzz@gmail.com>
2025-04-25 02:20:22 +0700

    Update MakePluginsJsonTask.kt

--- src/main/kotlin/com/crstlnz/komikchino/plugintools/tasks/MakePluginsJsonTask.kt
+++ src/main/kotlin/com/crstlnz/komikchino/plugintools/tasks/MakePluginsJsonTask.kt
@@ -47,8 +47,8 @@ abstract class MakePluginsJsonTask : DefaultTask() {
 
         // create repo.json file
         val providerInfo = extensions.findProvider()
-        if (providerInfo == null && komik == null) {
-            logger.lifecycle("Provider info not provided!")
+        if (providerInfo == null || komik == null) {
+            logger.lifecycle("Provider info or komik not provided!")
         }
 
         if (providerInfo != null && komik != null) {
