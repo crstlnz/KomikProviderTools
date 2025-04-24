1af3946be4fd5cebee53cfa01ca0f38080caa12c
Crytalenz <kelvinzzzzzzzzzz@gmail.com>
2025-04-25 02:48:59 +0700

    Update KomikPlugin.kt

--- src/main/kotlin/com/crstlnz/komikchino/plugintools/KomikPlugin.kt
+++ src/main/kotlin/com/crstlnz/komikchino/plugintools/KomikPlugin.kt
@@ -8,7 +8,7 @@ import com.crstlnz.komikchino.plugintools.configuration.registerConfigurations
 abstract class KomikPlugin : Plugin<Project> {
     override fun apply(project: Project) {
         project.extensions.create("komik", KomikExtension::class.java, project)
-        project.extensions.create("provider", ProviderInfo::class.java, project)
+        project.extensions.create("providerinfo", ProviderInfo::class.java, project)
 
         registerTasks(project)
         registerConfigurations(project)
